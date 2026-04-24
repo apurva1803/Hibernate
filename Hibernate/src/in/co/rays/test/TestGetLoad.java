95dcc31c1669da0d84e6a4de718d820e5ea17215
apurva1803 <apurva9raut.com>
2026-04-24 11:25:27 +0530

    done

---
+++ Hibernate/src/in/co/rays/test/TestGetLoad.java
@@ -0,0 +1,23 @@
+package in.co.rays.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import in.co.rays.user.UserDTO;
+
+public class TestGetLoad {
+
+	public static void main(String[] args) {
+
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
+		Session session = sf.openSession();
+
+		UserDTO dto = (UserDTO) session.load(UserDTO.class, 2);
+
+		// UserDTO dto = (UserDTO) session.get(UserDTO.class, 4);
+
+		System.out.println(dto);
+	}
+}
